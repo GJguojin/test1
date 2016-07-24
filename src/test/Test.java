8fb38ae37a987b012762beb4ae8769efd75d3690
GJguojin <1509667407@qq.com>
2016-07-24 11:28:14 +0800

    修改

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,6 @@ package test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		System.out.println("this is projirct for test git.");
+		System.out.println("this is projirct for test git...");
 	}
 }
