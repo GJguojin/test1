b13313ee5fc9a6c3f05f29b9121c7f3bc216e44b
GJguojin <1509667407@qq.com>
2016-07-24 11:18:59 +0800

    首次提交

---
+++ src/test/Test.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("this is projirct for test git.");
+	}
+}
